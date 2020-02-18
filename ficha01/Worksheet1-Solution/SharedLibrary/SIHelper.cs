e769e9d88fe96c7ec4661ac2b795de4f5e3003cb
André Paulo <andre_duivenvoorden@hotmail.com>
2020-02-18 13:21:43 +0000

    ficha 01

---
+++ ficha01/Worksheet1-Solution/SharedLibrary/SIHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary {
+    public class SIHelper {
+        public static string GetUCName() {
+            return "EI-SI 2019/2020";
+        }
+    }
+}
